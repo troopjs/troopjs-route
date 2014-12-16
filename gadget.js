a5e93aa45102388b6f528f3a5802bd99b7369cda
Eyal Arubas <eyalarubas@gmail.com>
2014-12-16 15:07:13 +0800

    convert indentation to spaces

--- gadget.js
+++ gadget.js
@@ -2,144 +2,144 @@
  * @license MIT http://troopjs.mit-license.org/
  */
 define([
-	"troopjs-core/component/gadget",
-	"./runner/sequence",
-	"when"
+    "troopjs-core/component/gadget",
+    "./runner/sequence",
+    "when"
 ], function (Gadget, sequence, when) {
-	"use strict";
+    "use strict";
 
-	/**
-	 * @class route.gadget
-	 * @extend core.component.gadget
-	 */
+    /**
+     * @class route.gadget
+     * @extend core.component.gadget
+     */
 
-	var ARRAY_PROTO = Array.prototype;
-	var ARRAY_PUSH = ARRAY_PROTO.push;
-	var ROUTE = "route";
-	var NAME = "name";
-	var TYPE = "type";
-	var VALUE = "value";
-	var ARGS = "args";
-	var RUNNER = "runner";
+    var ARRAY_PROTO = Array.prototype;
+    var ARRAY_PUSH = ARRAY_PROTO.push;
+    var ROUTE = "route";
+    var NAME = "name";
+    var TYPE = "type";
+    var VALUE = "value";
+    var ARGS = "args";
+    var RUNNER = "runner";
 
-	/**
-	 * Route set event
-	 * @localdoc Triggered when a route set is requested
-	 * @event route/set
-	 * @param {String} route Route
-	 * @param {Object} data Data
-	 * @param {...*} [args] Additional arguments
-	 * @preventable
-	 */
+    /**
+     * Route set event
+     * @localdoc Triggered when a route set is requested
+     * @event route/set
+     * @param {String} route Route
+     * @param {Object} data Data
+     * @param {...*} [args] Additional arguments
+     * @preventable
+     */
 
-	/**
-	 * Route change event
-	 * @localdoc Triggered when a route change is requested
-	 * @event route/change
-	 * @param {String} route Route
-	 * @param {String[]} data Data
-	 * @param {...*} [args] Additional arguments
-	 * @preventable
-	 */
+    /**
+     * Route change event
+     * @localdoc Triggered when a route change is requested
+     * @event route/change
+     * @param {String} route Route
+     * @param {String[]} data Data
+     * @param {...*} [args] Additional arguments
+     * @preventable
+     */
 
-	/**
-	 * Route change handler
-	 * @handler route/change
-	 * @inheritdoc #event-route/change
-	 * @localdoc Matches and executes route stored in data
-	 * @template
-	 * @return {*}
-	 */
+    /**
+     * Route change handler
+     * @handler route/change
+     * @inheritdoc #event-route/change
+     * @localdoc Matches and executes route stored in data
+     * @template
+     * @return {*}
+     */
 
-	/**
-	 * To change the current route
-	 * @event hub/route/set
-	 * @param {String} The new uri
-	 */
+    /**
+     * To change the current route
+     * @event hub/route/set
+     * @param {String} The new uri
+     */
 
-	/**
-	 * Runs routes
-	 * @ignore
-	 * @param {String} op Operation
-	 * @param {...*} [args] Additional arguments
-	 * @return {*} Result from last handler
-	 */
-	function runRoute(op) {
-		var me = this;
+    /**
+     * Runs routes
+     * @ignore
+     * @param {String} op Operation
+     * @param {...*} [args] Additional arguments
+     * @return {*} Result from last handler
+     */
+    function runRoute(op) {
+        var me = this;
 
-		// Prepare event object
-		var event = {};
-		event[TYPE] = ROUTE + "/" + op;
-		event[RUNNER] = sequence;
+        // Prepare event object
+        var event = {};
+        event[TYPE] = ROUTE + "/" + op;
+        event[RUNNER] = sequence;
 
-		// Modify first argument
-		arguments[0] = event;
+        // Modify first argument
+        arguments[0] = event;
 
-		// Delegate the actual emitting to emit
-		return me.emit.apply(me, arguments);
-	}
+        // Delegate the actual emitting to emit
+        return me.emit.apply(me, arguments);
+    }
 
-	return Gadget.extend({
-		"displayName" : "route/gadget",
+    return Gadget.extend({
+        "displayName" : "route/gadget",
 
-		/**
-		 * @handler
-		 * @inheritdoc
-		 * @localdoc Registers event handlers declared ROUTE specials
-		 */
-		"sig/initialize": function onInitialize() {
-			var me = this;
+        /**
+         * @handler
+         * @inheritdoc
+         * @localdoc Registers event handlers declared ROUTE specials
+         */
+        "sig/initialize": function onInitialize() {
+            var me = this;
 
-			return when.map(me.constructor.specials[ROUTE] || ARRAY_PROTO, function (special) {
-				return me.on(special[NAME], special[VALUE], special[ARGS][0] || undefined);
-			});
-		},
+            return when.map(me.constructor.specials[ROUTE] || ARRAY_PROTO, function (special) {
+                return me.on(special[NAME], special[VALUE], special[ARGS][0] || undefined);
+            });
+        },
 
-		/**
-		 * @handler hub/route/change
-		 * @param {String} uri The new URI.
-		 * Handles URI change to dispatch it to individual handlers by emitting a {@link #event-route/change} event
-		 * synchronously, call each handler when the pattern matches the new URI.
-		 */
-		"hub:memory/route/change": function onHashChange(uri) {
-			var me = this;
-			var args = [ "change" ];
-			ARRAY_PUSH.apply(args, arguments);
-			return me.task(function(resolve) {
-				resolve(runRoute.apply(me, args));
-			}, ROUTE + "/change");
-		},
+        /**
+         * @handler hub/route/change
+         * @param {String} uri The new URI.
+         * Handles URI change to dispatch it to individual handlers by emitting a {@link #event-route/change} event
+         * synchronously, call each handler when the pattern matches the new URI.
+         */
+        "hub:memory/route/change": function onHashChange(uri) {
+            var me = this;
+            var args = [ "change" ];
+            ARRAY_PUSH.apply(args, arguments);
+            return me.task(function(resolve) {
+                resolve(runRoute.apply(me, args));
+            }, ROUTE + "/change");
+        },
 
-		/**
-		 * Handles route set
-		 * @handler
-		 * @inheritdoc #event-route/set
-		 * @localdoc Republishes route on hub
-		 * @fires hub/route/set
-		 */
-		"route/set": function onRouteSet(route, data) {
-			var me = this;
-			var args = [ "route/set" ];
-			ARRAY_PUSH.apply(args, arguments);
-			return this.publish.apply(me, args);
-		},
+        /**
+         * Handles route set
+         * @handler
+         * @inheritdoc #event-route/set
+         * @localdoc Republishes route on hub
+         * @fires hub/route/set
+         */
+        "route/set": function onRouteSet(route, data) {
+            var me = this;
+            var args = [ "route/set" ];
+            ARRAY_PUSH.apply(args, arguments);
+            return this.publish.apply(me, args);
+        },
 
-		/**
-		 * Navigate to a new URI by fulfill the route parameters with the specified list of values, after emitting
-		 * a {@link #event-route/set} event synchronously, call each handler whose route pattern where the pattern matches it.
-		 *
-		 * @param {String} pattern The route pattern to construct the new route.
-		 * @param {Object} params The data object contains the parameter values for routing.
-		 * @return {Promise}
-		 * @fires route/set
-		 */
-		"go": function go(pattern, params) {
-			var me = this;
-			var args = [ "set" ];
-			ARRAY_PUSH.apply(args, arguments);
-			return me.task(function (resolve) {
-				resolve(runRoute.apply(me, args));
-			}, ROUTE + "/set");
-		}
-	});
+        /**
+         * Navigate to a new URI by fulfill the route parameters with the specified list of values, after emitting
+         * a {@link #event-route/set} event synchronously, call each handler whose route pattern where the pattern matches it.
+         *
+         * @param {String} pattern The route pattern to construct the new route.
+         * @param {Object} params The data object contains the parameter values for routing.
+         * @return {Promise}
+         * @fires route/set
+         */
+        "go": function go(pattern, params) {
+            var me = this;
+            var args = [ "set" ];
+            ARRAY_PUSH.apply(args, arguments);
+            return me.task(function (resolve) {
+                resolve(runRoute.apply(me, args));
+            }, ROUTE + "/set");
+        }
+    });
 });
